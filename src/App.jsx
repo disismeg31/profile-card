9436441ca25b1e4e92aa6a8446a4e05472481001
Megha KL <megha@nortvis.com>
2024-11-08 20:53:56 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+ 
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div className='text-[#55daa3] flex justify-center items-center'>Profile-Card</div>
+    </>
+  )
+}
+
+export default App
