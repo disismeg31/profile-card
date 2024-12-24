81f4387f09be039864c36839f24ccc9089ebee68
Megha KL <megha@nortvis.com>
2024-12-24 13:48:56 +0530

    Final changes for the project

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -6,33 +6,42 @@ function Profile() {
     
     <>
     <div className='w-80 rounded-xl bg-slate-100 flex flex-col justify-center items-center'>
-        <div className="section-1">
-            <img className='rounded-t-lg' src={backround} alt="" />
+        
+        <div className="section-1 w-full">
+            <img className='rounded-t-lg w-full' src={backround} alt="" />
         </div>
         <div className='section-2'>
-        <div className='img relative p-1 -top-[3.2rem] bg-slate-100 rounded-full'>
+           
+        <div className='img relative p-1 m-5 -mt-12 bg-slate-100 rounded-full'>
             <img className='rounded-full' src={propic} alt="" />
         </div>
-        <div className="name-age relative flex justify-center -top-10">
-            <p>Name</p>
-            <p>Age</p>
+         
+        <div className="name-age   flex justify-center mt-4 -top-20">
+            <p className='font-bold text-lg mr-2'>Victor Crest</p>
+            <p className='text-[#6a6f81] text-lg'>26</p>
         </div>
-        <p>Place</p>
+         
+        <p className='text-sm text-[#6a6f81] mt-3'>London</p>
         </div>
-        <hr className='text-black' />
-        <div className='section-3 flex justify-center items-center'>
-             <div>
-                <p>0</p>
-                <span>Followers</span>
+         
+        <hr className=' border-t border-[#dddbdb] w-full mt-5' />
+        {/* section-3 */}
+        <div className='w-full flex justify-around items-center mt-4 pb-4'>
+          <div className='flex justify-between relative'>
+             <div className='mx-8'>
+                <p className='font-bold'>80K</p>
+                <span className='text-xs text-[#6a6f81]'>Followers</span>
              </div>
-             <div>
-                <p>0</p>
-                <span>Likes</span>
+             <div className='mx-8'>
+                <p className='font-bold'>803K</p>
+                <span className='text-xs text-[#6a6f81]' >Likes</span>
              </div>
-             <div>
-                <p>0</p>
-                <span>Photos</span>
+             <div className='mx-8'>
+                <p className='font-bold'>1.4K</p>
+                <span className='text-xs text-[#6a6f81]' >Photos</span>
              </div>
+          </div>
+             
         </div>
         
     </div>
